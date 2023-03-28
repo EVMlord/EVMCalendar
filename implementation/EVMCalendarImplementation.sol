23fd1f5c3cc550ae21111c6b7df2263ab217d8c3
EVMlord <85406816+EVMlord@users.noreply.github.com>
2023-03-28 14:09:43 +0100

    Create EVMCalendarImplementation.sol

---
+++ implementation/EVMCalendarImplementation.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// @author EVMlord => https://lord.dev
+// All times are in UTC
+
+pragma solidity ^0.8.19;
+
+interface IEVMCalendar {
+    // @dev Converts timestamp to date string in YYYY-MM-DD HH:MM:SS format
+    function convertTimestamp(uint256 unixTimestamp)
+        external
+        pure
+        returns (string memory);
+
+    function countdown(uint256 endTimestamp)
+        external
+        view
+        returns (string memory);
+
+    // @dev Returns current time in HH:MM:SS format
+    function checkTimeUTC() external view returns (string memory);
+
+    function checkDateUTC() external view returns (string memory);
+
+    function getCurrentDateAndTimeUTC() external view returns (string memory);
+}
+
+contract EVMCalendarImplementation {
+    IEVMCalendar EVMSQL;
+
+    constructor(IEVMCalendar _EVMSQL) {
+        EVMSQL = _EVMSQL;
+    }
+
+    function convertTimestamp(uint256 unixTimestamp)
+        external
+        view
+        returns (string memory)
+    {
+        return EVMSQL.convertTimestamp(unixTimestamp);
+    }
+
+    function countdown(uint256 endTimestamp)
+        external
+        view
+        returns (string memory)
+    {
+        return EVMSQL.countdown(endTimestamp);
+    }
+
+    function checkTimeUTC() external view returns (string memory) {
+        return EVMSQL.checkTimeUTC();
+    }
+
+    function checkDateUTC() external view returns (string memory) {
+        return EVMSQL.checkDateUTC();
+    }
+
+    function getCurrentDateAndTimeUTC() external view returns (string memory) {
+        return EVMSQL.getCurrentDateAndTimeUTC();
+    }
+}
